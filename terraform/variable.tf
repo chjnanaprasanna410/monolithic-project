1bedc8abb0547e095d5037fae9373e874fe4f748
chjnanaprasanna410 <139343294+chjnanaprasanna410@users.noreply.github.com>
2024-01-07 19:58:37 +0530

    Create variable.tf
    created variable document

---
+++ terraform/variable.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+type =string
+default = "ami-09538990a0c4fe9be"
+}
+
+variable "itype" {
+type = string
+default ="t2.micro"
+}
+
+variable "kn" {
+type = string
+default = "project-keypair"
+}
+
+variable "iname" {
+type= strinf
+default = "Swiggy"
+}

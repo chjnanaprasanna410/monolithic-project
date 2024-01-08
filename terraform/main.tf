4e6bcf213ca6f1487cff5bfee6aac26be6ee259e
chjnanaprasanna410 <139343294+chjnanaprasanna410@users.noreply.github.com>
2024-01-08 20:43:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "key" {
 ami = "ami-012b9156f755804f5"
 instance_type = "t2.micro"
-key_name = "nwkpp"
+key_name = "Prassu"
 count = 1
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
